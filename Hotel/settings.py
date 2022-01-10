a525517809991f511588620e6c0b24980cb650c5
Luis Felipe Lassarotte <luisfelassa@gmal.com>
2022-01-10 13:02:26 -0300

    postgres

--- Hotel/settings.py
+++ Hotel/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Hotel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hotel',
+        'USER': 'hotelaria',
+        'PASSWORD': 'senha123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
